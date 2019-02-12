a00a667be2ea7e1a7fcfb45fe95c0323d1b7fcd2
Tyler Hunt <tyhunt04@gmail.com>
2019-02-12 05:30:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -73,22 +73,20 @@ def current_player(board)
 end
 
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combo|
-    win_index_1 = win_combo[0]
-    win_index_2 = win_combo[1]
-    win_index_3 = win_combo[2]
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
 
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
 
     if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
-        return true
-    else
-        return false
+      return win_combination
     end
   end
-  win_combo
+  return false
 end
 
 def full?(board)
