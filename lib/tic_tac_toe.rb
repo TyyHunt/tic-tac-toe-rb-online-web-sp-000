8454fe1b5f3c287bda2c1463d11d87cc5fcba7d6
Tyler Hunt <tyhunt04@gmail.com>
2019-02-12 05:15:02 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -86,9 +86,10 @@ def won?(board)
         return win_combo
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
         return win_combo
-      else
+    else
         return false
     end
+  end
 end
 
 def full?(board)
