8b72ecb922ab87b56475e006fe02fc85f224e2e5
Tyler Hunt <tyhunt04@gmail.com>
2019-02-12 05:04:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -81,11 +81,12 @@ def won?(board)
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
-
+  end
    if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
      return true
    else
      return false
+   end
 end
 
 def full?(board)
